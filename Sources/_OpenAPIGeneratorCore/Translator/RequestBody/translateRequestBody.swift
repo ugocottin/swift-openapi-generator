46e5954daf38f72052894dc2077bdf40fbeb20c2
Ugo Cottin <ugo.cottin@gmail.com>
2024-04-05 16:34:10 +0200

    Add XML coding strategy

--- Sources/_OpenAPIGeneratorCore/Translator/RequestBody/translateRequestBody.swift
+++ Sources/_OpenAPIGeneratorCore/Translator/RequestBody/translateRequestBody.swift
@@ -274,7 +274,7 @@ extension ServerFileTranslator {
                 )
             let bodyExpr: Expression
             switch codingStrategy {
-            case .json, .uri, .urlEncodedForm:
+            case .json, .xml, .uri, .urlEncodedForm:
                 // Buffering.
                 bodyExpr = .try(.await(converterExpr))
             case .binary, .multipart:
